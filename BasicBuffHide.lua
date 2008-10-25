7e1a240fdaf2d5c1e99682548a55e03aea74e355
funkydude <funkydude@users.noreply.github.com>
2008-10-25 20:47:41 +0000

    BasicBuffHide: Fix frame sometimes re-showing

--- BasicBuffHide.lua
+++ BasicBuffHide.lua
@@ -2,5 +2,9 @@
 ------------------BASIC BUFF HIDE-----------------
 --------------------------------------------------
 
+BuffFrame:UnregisterAllEvents()
 BuffFrame:Hide()
+BuffFrame.Show = function() end
+TemporaryEnchantFrame:UnregisterAllEvents()
 TemporaryEnchantFrame:Hide()
+TemporaryEnchantFrame.Show = BuffFrame.Show
